78f86caefb893c2012a6689c69ed298d9a30d9c7
Prabhat <singhprabhatp.s.224@gamil.com>
2022-03-16 20:36:16 +0530

    refactor: made some changes in my code according to PR

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,9 @@
 import React from 'react'
+import data from "../Data"
 import "./style.css"
 import './common.css'
 
 import landing_page from '../assests/images/marvel-tee-1.jpeg';
-import men_tshirt from '../assests/images/Marvel-Men-T-Shirt-1.jpeg';
-import hoodie_collection from '../assests/images/marvel-men-hoodie-1.jpeg';
-import women_tshirt from '../assests/images/marvel-women-tshirt-1.jpeg';
-import jacket_collection from "../assests/images/marvel-men-jacket-2.jpg"
 
 
 
@@ -23,50 +20,20 @@ const Home = () => {
                 </section>
                 <p className='text-lg text-center'>Collections</p>
                 <section className="container d-flex">
-                    <div className="card-img position-rel margin-xs">
-                        <img
-                            className="img-responsive"
-                            src={men_tshirt}
-                            alt="men_clothing"
-                        />
-                        <div className="card-badge-secondary position-abs top-50 left-0 padding-xs d-100 head-sm">
-                            <p className="btn-link"> Men Tshirt</p>
-                        </div>
-                    </div>
-                    <div className="card-img position-rel margin-xs">
-                        <img
-                            className="img-responsive"
-                            src={women_tshirt}
-                            alt="women_clothing"
-                        />
-                        <div className="card-badge-secondary position-abs top-50 left-0 padding-xs d-100 head-sm">
-                            <p className="btn-link">Women Tshirt</p>
-                        </div>
-                    </div>
-                    <div className="card-img position-rel margin-xs">
-                        <img
-                            className="img-responsive"
-                            src={hoodie_collection}
-                            alt="winter_collection"
-                        />
-                        <div className="card-badge-secondary position-abs top-50 left-0 padding-xs d-100 head-sm">
-                            <p className="btn-link">
-                                Hoodie Collection
-                            </p>
-                        </div>
-                    </div>
-                    <div className="card-img position-rel margin-xs">
-                        <img
-                            className="img-responsive"
-                            src={jacket_collection}
-                            alt="winter_collection"
-                        />
-                        <div className="card-badge-secondary position-abs top-50 left-0 padding-xs d-100 head-sm">
-                            <p className="btn-link">
-                                Jacket Collection
-                            </p>
-                        </div>
-                    </div>
+                    {
+                        data.map(({ id, image, name }) => (
+                            <div className="card-img position-rel margin-xs" key={id}>
+                                <img
+                                    className="img-responsive"
+                                    src={image}
+                                    alt="men_clothing"
+                                />
+                                <div className="card-badge-secondary position-abs top-50 left-0 padding-xs d-100 head-sm">
+                                    <p className="btn-link">{name}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </section>
             </main>
         </div>
