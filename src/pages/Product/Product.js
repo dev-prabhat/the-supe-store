aecf9e01cb4a7f57517a5125f08f084d6447fa20
Prabhat <singhprabhatp.s.224@gamil.com>
2022-03-26 18:24:14 +0530

    move image to public folder

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { ProductCard, Sidebar } from "../../components/index"
 import { useFilter } from '../../Context/Filter-Context'
+import { useProduct } from "../../Context/Product-Context"
 import "./product.css"
 import '../common.css'
 
@@ -8,16 +9,19 @@ import '../common.css'
 
 const Product = () => {
     const { filteredProduct } = useFilter()
+    const { loading } = useProduct()
     return (
         <main className="productpage-grid-container">
             <Sidebar />
-            <section className="productpage-content d-flex">
-                {
-                    filteredProduct().map((Obj) => (
-                        <ProductCard key={Obj._id} productObj={Obj} />
-                    ))
-                }
-            </section>
+            {
+                loading ? <div>Loading...</div> : <section className="productpage-content d-flex">
+                    {
+                        filteredProduct().map((Obj) => (
+                            <ProductCard key={Obj._id} productObj={Obj} />
+                        ))
+                    }
+                </section>
+            }
         </main>
     )
 }
