f19469d9e04e38994cbe1847806cc310bf1350e2
Prabhat <singhprabhatp.s.224@gamil.com>
2022-03-26 18:26:33 +0530

    mockman route implementation

---
+++ src/pages/Mock/mock.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Monkman from "mockman-js"
+import "./mock.css"
+
+export const Mock = () => {
+    return (
+        <main className="mock">
+            <Monkman />
+        </main>
+    )
+}
